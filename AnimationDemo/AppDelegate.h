806055a0e47954eaba1ef82c38067d791b223cc0
Rachel Bobbins <pair+rachelheidi@gmail.com>
2015-01-29 22:30:40 -0800

    Initial Commit

---
+++ AnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemo
+//
+//  Created by Rachel Bobbins on 1/29/15.
+//  Copyright (c) 2015 Rachel Bobbins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationDemo
+//
+//  Created by Rachel Bobbins on 1/29/15.
+//  Copyright (c) 2015 Rachel Bobbins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
